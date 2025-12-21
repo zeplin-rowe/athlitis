ef892dfde6db8404bf892354cb9f34bb3f8b002a
Zeplin Rowe <rowe_michael101@yahoo.com>
2025-12-20 21:37:11 -0500

    overhaul starting from scratch kinda

--- prisma/migrations/20251130202150_update_exercise_images/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `images` column on the `Exercise` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "Exercise" DROP COLUMN "images",
-ADD COLUMN     "images" TEXT[] DEFAULT ARRAY[]::TEXT[];
--- src/components/ui/Card.tsx
+++
@@ -1,9 +0,0 @@
-export default function Card({
-  children,
-  className = "",
-}: {
-  children: React.ReactNode;
-  className?: string;
-}) {
-  return <div className={`card ${className}`}>{children}</div>;
-}
